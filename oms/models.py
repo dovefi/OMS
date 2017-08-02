17bd4896b4249562e9ce69dbcc564019b9783dfa
dovefi <1762515982@qq.com>
2017-08-02 19:26:00 +0800

    add model history

--- oms/models.py
+++ oms/models.py
@@ -22,29 +22,47 @@ VB_MODULE_LIST = {
     'vb_mc' : 'module caption',
     'vb_me' : 'module extend',
 }
+VB_SERVER_HISTORY = {
+    'vb_hid' : 'history id',
+    'vb_hip' : 'history of the server',
+    'vb_hu'  : 'history user',
+    'vb_hd'  : 'happen date',
+    'vb_dd'  : 'insert into db date',
+    'vb_hc'  : 'history command',
+}
 
 # service function category
 class Server_fun_categ(models.Model):
-    server_categ_name = models.CharField(max_length=20, verbose_name=VB_SERVER_FUN_CATEG['vb_scn'])
+    server_categ_name = models.CharField(max_length=20, help_text=VB_SERVER_FUN_CATEG['vb_scn'])
 
 # service application category
 class Server_app_categ(models.Model):
-    server_fun_categ = models.ForeignKey(Server_fun_categ, on_delete=models.CASCADE, verbose_name=VB_SERVER_APP_CATEG['vb_sci'])
-    app_categ_name  = models.CharField(max_length=30, verbose_name=VB_SERVER_APP_CATEG['vb_acn'])
+    server_fun_categ = models.ForeignKey(Server_fun_categ, on_delete=models.CASCADE, help_text=VB_SERVER_APP_CATEG['vb_sci'])
+    app_categ_name  = models.CharField(max_length=30, help_text=VB_SERVER_APP_CATEG['vb_acn'])
 
 # list of servers
 class Server_list(models.Model):
-    server_name   = models.CharField(max_length=13, verbose_name=VB_SERVER_LIST['vb_sn'])
-    server_wip    = models.CharField(max_length=15, verbose_name=VB_SERVER_LIST['vb_sw'])
-    server_lip    = models.CharField(max_length=12, verbose_name=VB_SERVER_LIST['vb_sl'])
-    server_op     = models.CharField(max_length=10, verbose_name=VB_SERVER_LIST['vb_so'])
-    server_app_categ = models.ForeignKey(Server_app_categ, verbose_name=VB_SERVER_LIST['vb_sai'])
+    server_name   = models.CharField(max_length=13, help_text=VB_SERVER_LIST['vb_sn'])
+    server_wip    = models.CharField(max_length=15, help_text=VB_SERVER_LIST['vb_sw'])
+    server_lip    = models.CharField(max_length=12, help_text=VB_SERVER_LIST['vb_sl'])
+    server_op     = models.CharField(max_length=10, help_text=VB_SERVER_LIST['vb_so'])
+    server_app_categ = models.ForeignKey(Server_app_categ, help_text=VB_SERVER_LIST['vb_sai'])
 
 # list of module
 class Module_list(models.Model):
-    module_name    = models.CharField(max_length=20, verbose_name=VB_MODULE_LIST['vb_mn'])
-    module_caption = models.CharField(max_length=255, verbose_name=VB_MODULE_LIST['vb_mc'])
-    module_extend  = models.CharField(max_length=2000, verbose_name=VB_MODULE_LIST['vb_me'])
+    module_name    = models.CharField(max_length=20, help_text=VB_MODULE_LIST['vb_mn'])
+    module_caption = models.CharField(max_length=255, help_text=VB_MODULE_LIST['vb_mc'])
+    module_extend  = models.TextField(help_text=VB_MODULE_LIST['vb_me'])
+
+# server history
+class Server_history(models.Model):
+    history_id = models.IntegerField(help_text=VB_SERVER_HISTORY['vb_hid'])
+    server_list = models.ForeignKey(Server_list, help_text=VB_SERVER_HISTORY['vb_hip'])
+    history_user = models.CharField(max_length=15, help_text=VB_SERVER_HISTORY['vb_hu'])
+    history_datetime = models.DateTimeField(help_text=VB_SERVER_HISTORY['vb_hd'])
+    db_datetime = models.DateTimeField(help_text=VB_SERVER_HISTORY['vb_dd'])
+    history_command = models.CharField(max_length=255, help_text=VB_SERVER_HISTORY['vb_hc'])
+
 
 
 
